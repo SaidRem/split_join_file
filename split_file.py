cd25eed1b2bff9d3b9d78286acb733ed874dcfc5
SaidRem <said.r@yandex.ru>
2021-03-30 19:47:25 +0400

    Added functionality and comments

--- split_file.py
+++ split_file.py
@@ -6,19 +6,20 @@ SIZE_PART = 2**20 * 10   # 10 Mb
 def split_file(the_file, to_dir, size_part=SIZE_PART):
     """
     The function takes as arguments:
-    the_file - path to file for split to parts
-    to_dir - target directory for store splitted file
-    optional arguments:
-    size_part - size of part of the file 
+    1 - the path to the file;
+    2 - folder path for parts of the file;
+    3 - file part size (default: SIZE_PART (10 mb));
     """
-    if os.path.exists(the_file):                       # check for path exists to the file
+    if os.path.isfile(the_file):                       # check for path exists to the file
         file_size = os.path.getsize(the_file)          # 
-        if file_size/SIZE_PART > 9999:
-            return f'The file too large to be split into {SIZE_PART} byte chunks.'
+        if file_size/SIZE_PART > 999:
+            return f'The file is too large to split into {SIZE_PART} byte parts.'
+    else:
+        print('Enter correct path to a file')
     with open(the_file, 'rb') as the_f:
         i = 0
         while True:
-            part_name = 'part_{:04}'.format(i)
+            part_name = 'part_{:03}'.format(i)
             i += 1
             with open(os.path.join(to_dir, part_name), 'wb') as par_f:
                 p = the_f.read(SIZE_PART)
@@ -31,3 +32,6 @@ if __name__ == '__main__':
         the_file = input('Enter path to file for split')
         to_dir = input('Enter target direcotry for storing parts of the splitting file')
         split_file(the_file, to_dir)
+    else:
+        the_file, to_dir = sys.argv[1:3]
+        
