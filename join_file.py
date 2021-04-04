bd6e93efe8afcb840bf4bae7f7bd459446c00a51
SaidRem <said.r@yandex.ru>
2021-04-04 21:34:49 +0400

    Added functionality for join parts of files

--- join_file.py
+++ join_file.py
@@ -2,16 +2,25 @@ import os
 import sys
 
 
-READ_CHUNK = 262144
+READ_CHUNK = 2*20 * 10
 
-def join(dir_name, file_name):
+def join_parts(dir_name, file_name):
     if not os.path.exists(dir_name):
         print('Enter correct path to the directory')
     file_parts = os.listdir(dir_name)
     file_parts.sort()
-    with open(file_name, 'wb') as col_file:
+    with open(file_name, 'wb') as the_f:
         for name in file_parts:
-            path_to = os.path.join(dir_name, name)
-            with open('path_to', 'rb') as p_file:
+            path_parts = os.path.join(dir_name, name)
+            with open(path_parts, 'rb') as part_n:
                 while True:
-                    break
+                    part_bytes = part_n.read(READ_CHUNK)
+                    if not part_bytes:
+                        break
+                    the_f.write(part_bytes)
+
+
+if __name__ == '__main__':
+    dir_name = input('Enter the path to the file parts for join\n')
+    name = input('Enter the name of new file\n')
+    join_parts(dir_name, name)
