382375846f668b58411f0940ba6673378da2ca98
SaidRem <said.r@yandex.ru>
2021-04-11 15:05:14 +0400

    Join files bigger than 100 mb

--- join_file.py
+++ join_file.py
@@ -4,6 +4,7 @@ import re
 
 
 READ_CHUNK = 2*20 * 10    # 2*20 = 1 Mb
+ONE_HUND = 2*20 * 100  # 100 Mb
 
 def test_parts(file_parts):
     """
@@ -15,7 +16,7 @@ def test_parts(file_parts):
         if not pat.match(p):
             return p
 
-def join_parts(dir_name, file_name):
+def join_parts(dir_name, file_name, tar_dir=''):
     if not os.path.exists(dir_name):
         print('Enter correct path to the directory')
     file_parts = os.listdir(dir_name)
@@ -23,19 +24,27 @@ def join_parts(dir_name, file_name):
     testing = test_parts(file_parts)
     if testing:
         return f'Incorrect file: {testing}'
-    with open(file_name, 'wb') as the_f:
+    with open(os.path.join(tar_dir, file_name), 'wb') as the_f:
         for name in file_parts:
-            path_parts = os.path.join(dir_name, name)
-            with open(path_parts, 'rb') as part_n:
-                while True:
-                    part_bytes = part_n.read(READ_CHUNK)
-                    if not part_bytes:
-                        break
-                    the_f.write(part_bytes)
-    return "File build was successful"
+            path_part = os.path.join(dir_name, name)
+            with open(path_part, 'rb') as part_n:
+                if os.path.getsize(path_part) > ONE_HUND:
+                    while True:
+                        part_bytes = part_n.read(ONE_HUND)
+                        if not part_bytes:
+                            break
+                        the_f.write(part_bytes)
+                else:
+                    while True:
+                        part_bytes = part_n.read(READ_CHUNK)
+                        if not part_bytes:
+                            break
+                        the_f.write(part_bytes)
+    return "File compiled successfully"
 
 
 if __name__ == '__main__':
     dir_name = input('Enter the path to the file parts for join\n')
     name = input('Enter the name of new file\n')
-    print(join_parts(dir_name, name))
+    tar_dir = input('Enter target directory\n')
+    print(join_parts(dir_name, name, tar_dir))
